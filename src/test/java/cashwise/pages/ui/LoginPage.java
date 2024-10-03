9055d2d17774a288e4a6d3811491880e1df52b26
codewiser116 <codewise.academy@gmail.com>
2024-10-03 17:45:26 -0500

    deleted logs

--- src/test/java/cashwise/pages/ui/LoginPage.java
+++ src/test/java/cashwise/pages/ui/LoginPage.java
@@ -3,6 +3,8 @@ package cashwise.pages.ui;
 import cashwise.base.BasePage;
 import cashwise.utils.Driver;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage extends BasePage {
@@ -12,8 +14,23 @@ public class LoginPage extends BasePage {
     public LoginPage(WebDriver driver){
         super(driver);
         this.driver = driver;
-        PageFactory.initElements(driver, LoginPage.class);
+        PageFactory.initElements(driver, this);
     }
+
+    @FindBy(xpath = "//button[text()='Sign up']")
+    public WebElement signUpBtn;
+
+    @FindBy(id = "email_input_text")
+    public WebElement emailField;
+
+    @FindBy(id = "password_input_text")
+    public WebElement passwordField;
+
+    @FindBy(id = "repeat_password_input_text")
+    public WebElement confirmPasswordField;
+
+    @FindBy(id = "//button[text()='Continue']")
+    public WebElement continueBtn;
 
 
 
